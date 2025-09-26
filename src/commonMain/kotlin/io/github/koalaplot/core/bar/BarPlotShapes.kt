e80142fa98f4b070647a8b70381de0d6b20fe1b0
Peter Getek <postfixNotation@gmail.com>
2025-09-26 09:31:00 +0200

    adding additional concaveConvex shape which adds additional convex cutout at the bottom; delegates to ConcaveConvexShape implementation

--- src/commonMain/kotlin/io/github/koalaplot/core/bar/BarPlotShapes.kt
+++ src/commonMain/kotlin/io/github/koalaplot/core/bar/BarPlotShapes.kt
@@ -152,3 +152,91 @@ public class ConcaveConvexShape<X, E : VerticalBarPlotEntry<X, Float>>(
         }.let(Outline::Generic)
     }
 }
+
+/**
+ * Rectangle shape with concave/convex shaped sides.
+ * Useful for Single Vertical Bar and Stacked Bars Plot rendering.
+ *
+ * Primary constructor:
+ * @param concaveConvexShape The internal shape logic used for rendering.
+ * @param value The [VerticalBarPlotEntry] that defines the cutouts for the [ConcaveConvexShape].
+ *
+ * Secondary constructor:
+ * @param xyGraphScope Provides access to [yAxisModel] and acts as an implementation of [XYGraphScope].
+ * @param value The [VerticalBarPlotEntry] used to construct the internal shape as well as the additional convex cutout.
+ */
+@Stable
+public class ConvexConcaveConvexShape<X, E : VerticalBarPlotEntry<X, Float>> private constructor(
+    private val concaveConvexShape: ConcaveConvexShape<X, E>,
+    private val value: E
+) : Shape, XYGraphScope<X, Float> by concaveConvexShape {
+
+    public constructor(
+        xyGraphScope: XYGraphScope<X, Float>,
+        value: E
+    ) : this(
+        ConcaveConvexShape(xyGraphScope, value),
+        value
+    )
+
+    override fun createOutline(
+        size: Size,
+        layoutDirection: LayoutDirection,
+        density: Density
+    ): Outline {
+        val shapeWidth = size.width
+        val shapeHeight = size.height
+        val arcRadius = shapeWidth / 2
+
+        // Rendering negative values
+        val isInverted = value.y.yMax < value.y.yMin
+        val yZeroOffset = yAxisModel.computeOffset(0F).coerceIn(0F, 1F)
+        val yMinOffset = yAxisModel.computeOffset(value.y.yMin).coerceIn(0f, 1f)
+        val yMaxOffset = yAxisModel.computeOffset(value.y.yMax).coerceIn(0f, 1f)
+
+        // Prevent division by zero
+        if (yMaxOffset == yMinOffset) return Path().let(Outline::Generic)
+
+        val heightOffsetRatio = size.height / (yMaxOffset - yMinOffset)
+        val offsetToHeight = { offset: Float -> offset * heightOffsetRatio }
+
+        val yMaxZeroOffset = yMaxOffset - yZeroOffset
+        val yMaxZeroHeight = offsetToHeight(yMaxZeroOffset)
+
+        val outline = concaveConvexShape
+            .createOutline(size, layoutDirection, density)
+        outline as Outline.Generic
+
+        val cutoutRect = Path().apply {
+            addRect(
+                rect = Rect(
+                    offset = Offset(0F, yMaxZeroHeight - arcRadius),
+                    size = Size(shapeWidth, shapeWidth)
+                )
+            )
+        }
+        val cutoutArc = Path().apply {
+            addArc(
+                oval = Rect(
+                    offset = Offset(0F, yMaxZeroHeight - shapeWidth),
+                    size = Size(shapeWidth, shapeWidth)
+                ),
+                startAngleDegrees = 0F,
+                sweepAngleDegrees = 180F
+            )
+        }
+        val cutout = (cutoutRect - cutoutArc).apply {
+            // Rendering bar in negative direction
+            if (isInverted) {
+                Matrix().apply {
+                    resetToPivotedTransform(
+                        pivotX = shapeWidth / 2F,
+                        pivotY = shapeHeight / 2F,
+                        rotationZ = 180F
+                    )
+                }.let(::transform)
+            }
+        }
+        return (outline.path - cutout).let(Outline::Generic)
+    }
+}
